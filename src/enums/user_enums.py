640248dd4aba601d893d894ba451d7350d009f09
BogdanFoujin <162558962+BogdanFoujin@users.noreply.github.com>
2024-03-25 17:52:44 +0300

    add builder

--- src/enums/user_enums.py
+++ src/enums/user_enums.py
@@ -1,12 +1,12 @@
 from enum import Enum
 
 class Genders(Enum):
-    female = "female"
-    male="male"
+    FEMALE = "female"
+    MALE="male"
 
 class Statuses(Enum):
-    inactive = "inactive"
-    active = "active"
+    INACTIVE = "inactive"
+    ACTIVE = "active"
 
 class UsersErrors(Enum):
     WRONG_EMAIL = "Email doesnt contain @"
